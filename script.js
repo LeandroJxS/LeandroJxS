0b8275b67580b916de43d7d1b0eed89cc01c4ca6
LeandroJxS <Leandro22.JS@gmail.com>
2022-01-25 12:10:20 -0300

    ProjetinhoVersao3.1

---
+++ script.js
@@ -0,0 +1,24 @@
+class Produto {
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+
+    }
+
+    adicionar() {
+        alert('Produto Adicionado');
+        // ....
+    }
+
+    excluir() {
+
+        // ..
+
+
+        alert('Deletado!')
+    }
+}
+
+var produto = new Produto();
